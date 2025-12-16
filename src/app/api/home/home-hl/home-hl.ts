efcde82262bd3c1727d23458f776b03bdd369355
Alez05 <cercel.alexandru436@gmail.com>
2025-12-16 21:39:09 +0100

    feat: edited the home page hero

--- src/app/api/home/home-hl/home-hl.ts
+++ src/app/api/home/home-hl/home-hl.ts
@@ -3,7 +3,7 @@ import { THero } from "../../../../components";
 export const heroData: THero = {
   name: "Alexandru Cercel",
   role: "Fullstack Web Developer",
-  text: "Building performant, scalable, and maintainable web applications from concept to deployment.",
+  text: "I’m a fullstack developer with a strong passion for building performant, scalable, and user-friendly applications. I enjoy turning ideas into reliable digital products, focusing on clean code, thoughtful architecture, and smooth user experiences.",
   location: "London / Amsterdam",
   email: "cercel.alexandru436@gmail.com",
   phone: "+44 7770282405",
