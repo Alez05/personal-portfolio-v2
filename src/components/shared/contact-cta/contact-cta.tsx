4e09fc852716f20fd56f9f3850cf281ddbbda145
Alez05 <cercel.alexandru436@gmail.com>
2026-02-16 20:46:16 +0100

    feat: changed some styling on the contact hero, added more color and bit more styling

--- src/components/shared/contact-cta/contact-cta.tsx
+++ src/components/shared/contact-cta/contact-cta.tsx
@@ -1,13 +1,22 @@
-import './contact-cta.css'
+import "./contact-cta.css";
 
 const ContactCTA = () => {
   return (
-    <div className='ct-wrapper'>
-      <h1 className='ct-title'>Let's connect!</h1>
-      <p className='ct-description'>Interested in colaborations, team projects, and just building your network? I am happy to connect!</p>
-      <a className="ct-link" href="/contact">Contact me</a>
-    </div>
-  )
-}
+    <section className="ct-section">
+      <div className="ct-wrapper">
+        <h1 className="ct-title">Let’s connect!</h1>
 
-export { ContactCTA }
+        <p className="ct-description">
+          Interested in collaborations, team projects, or just expanding your
+          network? I’d be happy to connect with you.
+        </p>
+
+        <a className="ct-link" href="/contact">
+          Contact me
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export { ContactCTA };
