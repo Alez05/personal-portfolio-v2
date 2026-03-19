2ed787d6ee87a1d1939c3161c3c132617ed469fd
Alez05 <cercel.alexandru436@gmail.com>
2026-03-19 23:48:46 +0100

    feat: skills-preview new features - looking into the possibility to extend and design the components

--- src/components/home/skills-preview/skills-preview.type.ts
+++ src/components/home/skills-preview/skills-preview.type.ts
@@ -1,14 +1,16 @@
-export type TSkill = {
-  name: string;
+export type TExpertiseCard = {
+  title: string;
   icon: string;
+  subtitle: string;
+  points: string[];
 };
 
 export type TSkillsPreview = {
   title: string;
   description?: string;
-  skills: TSkill[];
+  cards: TExpertiseCard[];
   cta?: {
-    text: string;
+    text?: string;
     label: string;
     link: string;
   };
